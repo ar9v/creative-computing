4a646efaf34184d6893f9d00e0123f7882a22f6c
Ricardo Vela <aricav96@gmail.com>
2020-11-12 13:36:03 -0600

    Restructure site. Add pages, remove in-place DOM manipulation.
    To keep things simpler with JS, I simply decided to make different
    pages. I have made a decent-looking grid, but the text alignment
    needs to change to bottom for better visibility. Using most of the
    remaining space would be great.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,6 @@ module.exports = {
   },
   purge: [],
   theme: {
-	  screens: {
-		  sm: '640px',
-		  md: '768px',
-		  lg: '1024px',
-		  xl: '1280px',
-      },
       extend: {
 		  colors: {
 			  black: "#111",
