114e3221d761930f79b960dccb63a50c50d18c73
Ricardo Vela <aricav96@gmail.com>
2020-11-23 13:05:59 -0600

    Trying to change the name to see if GHPages picks this up...

---
+++ docs/pmodal.js
@@ -0,0 +1,88 @@
+import { noteSketch, hueSketch } from './sketch.js'
+
+// Check for the artist, to know which json to load
+let jsonName = document.querySelector('body').id
+let titleElem = document.querySelector('#workTitle')
+let descrElem = document.querySelector('#description')
+let explElem = document.querySelector('#explanation')
+let modalImage = document.querySelector('#modalImage')
+let sketchHolder = document.querySelector("#sketch-holder")
+let link = document.querySelector('#link')
+let pageData
+var hsk
+var osk
+
+// Get the page data
+fetch(`text/${jsonName}`)
+	.then(response => response.json())
+	.then(data => pageData = data);
+
+// Fill in the information with the page data
+function fillModal(element) {
+	// This should actually be undefined-checked
+	let info = pageData[element.id]
+
+	titleElem.innerHTML = info.title
+	descrElem.innerHTML = info.description
+	explElem.innerHTML = info.explanation
+
+	// modalImage may be null because
+	// processing doesn't use an image
+	if(modalImage != undefined) {
+		modalImage.src = info.bg
+	}
+
+	if(sketchHolder) {
+		if(element.id == "brightness") {
+			// new p5(brightSketch, 'sketch-holder')
+		}
+		else if(element.id == "hue") {
+			hsk = new p5(hueSketch, 'sketch-holder')
+			// new p5(hueSketch, 'sketch-holder')
+		}
+		else { 	// element.id == "oscillator"
+			osk = new p5(noteSketch, 'sketch-holder')
+		}
+	}
+
+	link.href = info.link
+}
+
+// Modal controls
+function toggleModal () {
+	const modal = document.querySelector('.modal')
+    const body = document.querySelector('body')
+    modal.classList.toggle('opacity-0')
+    modal.classList.toggle('pointer-events-none')
+    body.classList.toggle('modal-active')
+}
+
+let modalOpeners = document.querySelectorAll('.modal-open')
+modalOpeners.forEach(opener => {
+	opener.addEventListener('click', event => {
+		event.preventDefault()
+		toggleModal()
+		fillModal(event.currentTarget)
+	});
+});
+
+const overlay = document.querySelector('.modal-overlay')
+overlay.addEventListener('click', () => { toggleModal(); cleanUp(); })
+
+let modalClosing = document.querySelectorAll('.modal-close')
+modalClosing.forEach(closingElement => {
+	closingElement.addEventListener('click', () => {
+		toggleModal()
+		cleanUp()
+	});
+});
+
+// Canvas manipulation
+function cleanUp() {
+	document.querySelector("canvas").remove()
+	osk.remove()
+}
+
+// new p5(brightSketch, 'sketch-holder')
+// let hsketch = new p5(hueSketch, 'sketch-holder')
+// let nsketch = new p5(noteSketch, 'sketch-holder')
---
+++ docs/sketch.js
@@ -0,0 +1,101 @@
+p5.disableFriendlyErrors = true; // disables FES
+export let hueSketch = (p, n) => {
+	p.barWidth = 20;
+	p.lastBar = -1;
+	p.canvasWidth = document.querySelector("#sketch-holder").offsetWidth;
+	p.canvasHeight = document.querySelector("#sketch-holder").offsetHeight;
+
+	p.setup = function() {
+		p.createCanvas(p.canvasWidth, p.canvasHeight);
+		p.colorMode(p.HSB, p.height, p.height, p.height);
+		p.noStroke();
+		p.background(0);
+	}
+
+	p.draw = function () {
+		p.whichBar = p.mouseX / p.barWidth;
+		if (p.whichBar !== p.lastBar) {
+			p.barX = p.whichBar * p.barWidth;
+			p.fill(p.mouseY, p.height, p.height);
+			p.rect(p.barX, 0, p.barWidth, p.height);
+			p.lastBar = p.whichBar;
+		}
+	}
+};
+
+export let noteSketch = (p, n) => {
+
+	p.scaleArray = [60, 62, 64, 65, 67, 69, 71, 72];
+	p.canvasWidth = document.querySelector("#sketch-holder").offsetWidth;
+	p.canvasHeight = document.querySelector("#sketch-holder").offsetHeight;
+	p.noteKeys = {
+		'a': 60,
+		'w': 61,
+		's': 62,
+		'e': 63,
+		'd': 64,
+		'f': 65,
+		't': 66,
+		'h': 67,
+		'u': 68,
+		'j': 69,
+		'i': 70,
+		'k': 71,
+		'l': 72
+	}
+	p.note = 0;
+
+	p.setup = function () {
+		p.createCanvas(p.canvasWidth, p.canvasHeight);
+		p.osc = new p5.SinOsc();
+
+		// Instantiate the envelope
+		p.envelope = new p5.Env();
+
+		// set attackTime, decayTime, sustainRatio, releaseTime
+		p.envelope.setADSR(0.001, 0.5, 0.1, 0.5);
+
+		// set attackLevel, releaseLevel
+		p.envelope.setRange(1, 0);
+
+
+		p.fft = new p5.FFT();
+		p.noStroke();
+	}
+
+	p.draw = function() {
+		p.background(20);
+
+		// plot FFT.analyze() frequency analysis on the canvas
+		p.spectrum = p.fft.analyze();
+		for (p.i = 0; p.i < p.spectrum.length / 20; p.i++) {
+			p.fill(p.spectrum[p.i], p.spectrum[p.i] / 10, 0);
+			p.x = p.map(p.i, 0, p.spectrum.length / 20, 0, p.width);
+			p.h = p.map(p.spectrum[p.i], 0, 255, 0, p.height);
+			p.rect(p.x, p.height, p.spectrum.length / 20, -p.h);
+		}
+	}
+
+	p.keyPressed = function() {
+		let valueToPlay = p.noteKeys[p.key]
+		p.osc.start();
+		
+		if(valueToPlay != undefined) {
+			// It's a letter key, play a sound
+			p.midiValue = valueToPlay
+			p.freqValue = p.midiToFreq(p.midiValue);
+			p.osc.freq(p.freqValue);
+
+			p.envelope.play(p.osc, 0, 0.1);
+
+			p.spectrum = p.fft.analyze();
+			for (let i = 0; i < p.spectrum.length / 20; i++) {
+				p.fill(p.spectrum[i], p.spectrum[i] / 10, 0);
+				p.x = p.map(i, 0, p.spectrum.length / 20, 0, p.width);
+				p.h = p.map(p.spectrum[i], 0, 255, 0, p.height);
+				p.rect(p.x, p.height, p.spectrum.length / 20, -p.h);
+			}
+		}
+	}
+
+};
